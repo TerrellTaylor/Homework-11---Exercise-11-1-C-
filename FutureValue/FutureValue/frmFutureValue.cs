e9526f7cbdff7fb63002a2af4bbe31c453d183e1
Terrell Taylor <ttaylo59@smartweb.augustatech.edu>
2026-05-01 23:57:02 -0400

    commit

--- FutureValue/FutureValue/frmFutureValue.cs
+++ FutureValue/FutureValue/frmFutureValue.cs
@@ -22,7 +22,7 @@ namespace FutureValue
 
                 decimal futureValue = CalculateFutureValue(monthlyInvestment, monthlyInterestRate, months);
 
-                txtFutureValue.Text = futureValue.ToString("p");
+                txtFutureValue.Text = futureValue.ToString("c");
                 txtMonthlyInvestment.Focus();
             }
             catch (Exception ex)
